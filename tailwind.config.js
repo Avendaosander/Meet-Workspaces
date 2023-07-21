96585aaf1e6d6b5f2751b0fedc1b8c4d5e5f8c10
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-07-21 13:31:53 -0400

    Clases agregadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,18 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        'min-500': '500px',
+        'min-900': '900px'
+      },
 			fontFamily: {
 				Laila: ["Laila", "sans-serif"],
 				Poppins: ["Poppins", "sans-serif"],
 				Raleway: ["Raleway", "sans-serif"],
 			},
+      backgroundImage: {
+        'home-section': "url('./src/assets/Sala.jpg')",
+      },
       boxShadow: {
         'cards': '-4px 4px 10px 0px rgba(3, 105, 161, 0.5)',
       }
