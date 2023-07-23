6ecb03f8b5690d25e3611b27dfd54d86b94a593b
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-07-23 12:44:20 -0400

    Apollo/client agregado

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { UserProvider } from './context/userContext.jsx'
 import './index.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+const { VITE_URL_BACKEND } = import.meta.env
+
+const client = new ApolloClient({
+  uri: VITE_URL_BACKEND,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <ApolloProvider client={client}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </ApolloProvider>
   </React.StrictMode>,
 )
