6bfc5c1490a0370fe7ac6c312b2cbb88878055b9
Alejo2608 <bricenovalecillose@uvm.edu.ve>
2023-07-28 19:02:16 -0400

    Formulario de reserva

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
    const {pathname} = useLocation()
    const pinUp = pathname === '/reservations' || pathname === '/dashboard'
 	return (
-		<footer className={`${pinUp && 'absolute bottom-0'} grid md:grid-cols-2 lg:grid-cols-3 bg-sky-700 text-sky-50 text-center gap-10 p-5 md:p-10 w-full`}>
+		<footer className={`${pinUp && 'absolute bottom-0'} grid md:grid-cols-2 lg:grid-cols-3 bg-sky-700 text-sky-50 text-center gap-10 p-5 md:p-10 w-full place-items-center`}>
 			<section className='md:row-start-1 lg:row-auto flex flex-col gap-5'>
 				<strong className='text-xl'>Meet Workspace</strong>
 				<p className='text-lg'>Todos los derechos reservados © 2023</p>
@@ -19,8 +19,11 @@ function Footer() {
             </a>
          </section>
 			<section className='md:row-start-1 lg:row-auto flex flex-col gap-5'>
-            <strong className='text-xl'>Desarrollador</strong>
-            <a href='https://github.com/avendaosander' target='_blank' rel="noopener noreferrer" className='text-lg hover:scale-110'>Alexander Avendaño</a>
+            <strong className='text-xl'>Desarrolladores</strong>
+            <div className='flex flex-col gap-1'>
+               <a href='https://github.com/avendaosander' target='_blank' rel="noopener noreferrer" className='text-lg hover:scale-110'>Alexander Avendaño</a>
+               <a href='https://github.com/alejo2608' target='_blank' rel="noopener noreferrer" className='text-lg hover:scale-110'>Eutimio Briceño</a>
+            </div>
          </section>
 		</footer>
 	)
